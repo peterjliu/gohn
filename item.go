844b4c8f055f5206b488fffbebeb8ba2af9781c4
peterliu <peterjliu@gmail.com>
2015-07-02 12:58:53 -0700

    update to use protocol buffers

--- item.go
+++ item.go
@@ -1,26 +1,13 @@
 package gohn
 
-import "fmt"
-
-type Item struct {
-	By          string `json:"by"`
-	Descendants int    `json:"descendants"`
-	ID          int    `json:"id"`
-	Kids        []int  `json:"kids"`
-	Parent      int    `json:"parent"`
-	Parts       []int  `json:"kids"`
-	Score       int    `json:"score"`
-	Text        string `json:"text"`
-	Time        int    `json:"time"`
-	Title       string `json:"title"`
-	Type        string `json:"type"`
-	URL         string `json:"url"`
-}
+import (
+	"fmt"
+)
 
 func (i *Item) PrettyString() string {
 	var p string
-	if i.Type == "story" {
-		p = fmt.Sprintf("%d: %s", i.ID, i.Title)
+	if *i.Type == "story" {
+		p = fmt.Sprintf("%d: %s", *i.Id, *i.Title)
 	} else {
 		p = "error"
 	}
